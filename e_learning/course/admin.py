4fda857053d5ac1e1f79d67e04275e2090ebdb87
vano <vanuartw@mail.ru>
2023-12-04 19:12:09 +0300

    admin register

--- e_learning/course/admin.py
+++ e_learning/course/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Course, Subject, Module
+
 
 # Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class InlineModule(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['subject', 'created']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [InlineModule]
