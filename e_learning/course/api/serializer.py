0e20c58461f0de30c6658885ca7ad21d3c23ee1e
vano <vanuartw@mail.ru>
2023-12-18 10:28:34 +0300

    rest api final

--- e_learning/course/api/serializer.py
+++ e_learning/course/api/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from course.models import Subject, Course, Module
+from course.models import Subject, Course, Module, Content
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -20,4 +20,36 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner',
+                  'module_created']
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description',
+                  'contents']
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    module_created = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug',
+                  'overview', 'created', 'owner',
                   'module_created']
--- e_learning/course/api/urls.py
+++ e_learning/course/api/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import view
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('course', view.ViewSetCourse)
 
 
 urlpatterns = [
     path('subjects/', view.SubjectListView.as_view(), name='subject_list'),
     path('subjects/<int:pk>/', view.SubjectDetailView.as_view(), name='subject_detail'),
-    path('courses/<int:pk>/enroll/', view.CourseEnrollView.as_view(), name='course_enroll')
+    path('', include(router.urls)),
 
 ]
